f572290a7b826c82615d26bba640b41b079ff5f8
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2024-01-31 07:17:07 +0900

    '#'4 Storeからtaskを参照してコンポーネントのpropsにした

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,28 @@
   <main>
     <header>
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
-      <h1>pinia tasks</h1>
+      <h1>Pinia Tasks</h1>
     </header>
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <P>{{ title }}</P>
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import {useTaskStore}from './stores/TaskStore'
+import TaskDetails from './components/TaskDetails.vue'
 export default {
-
+  components: {TaskDetails},
+setup() {
+  const taskStore = useTaskStore()
+  return {taskStore}
+}
 }
 </script>
+
+<!-- taskStoreはTaskStore.jsのstateのなかのもの -->
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia';
+export const useTaskStore = defineStore('taskStore', {
+  state: () => ({
+    tasks: [
+      { id: 1, title: "buy some milk", isFav: false },
+      { id: 2, title: "play ZELDA", isFav: true },
+    ]
+  })
+})
