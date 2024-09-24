1010ec24062b6d926b97add36c6eae67f4f99586
kyokahayashi <kyoka.hayashi@rakus-partners.co.jp>
2024-09-24 21:58:24 +0900

    fav機能、全てのタスクリストとお気に入りリストの表示切替

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -5,5 +5,18 @@ export const useTaskStore = defineStore('taskStore', {
       { id: 1, title: "buy some milk", isFav: false },
       { id: 2, title: "play ZELDA", isFav: true },
     ]
-  })
+  }),
+  getters: {
+    favs() {
+      return this.tasks.filter(t => t.isFav)
+    },
+    favCount() {
+      return this.tasks.reduce((p, c) => {
+        return c.isFav ? p + 1 : p
+      }, 0)
+    },
+    totalCount: (state) => {
+      return state.tasks.length
+    }
+  }
 })
